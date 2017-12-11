7e737f6de59e79b0ca810998f62d5f001629dc33
Real Solutions Luxembourg <erp@real.lu>
2017-12-11 11:27:55 +0100

    checkout the branch/tag and do not detach from HEAD when cloning

--- src/pip/_internal/vcs/git.py
+++ src/pip/_internal/vcs/git.py
@@ -170,7 +170,7 @@ class Git(VersionControl):
         else:
             self.run_command(['fetch', '-q'], cwd=dest)
         # Then reset to wanted revision (maybe even origin/master)
-        rev_options_detached = self.check_rev_options(dest, rev_options)
+        rev_options = self.check_rev_options(dest, rev_options)
         cmd_args = ['reset', '--hard', '-q'] + rev_options.to_args()
         self.run_command(cmd_args, cwd=dest)
         #: update submodules
@@ -187,14 +187,16 @@ class Git(VersionControl):
             self.run_command(['clone', '-q', url, dest])
 
             if rev:
-                rev_options_detached = self.check_rev_options(dest, rev_options)
+                # original branch/tag/revision from link url
+                revision = rev_options.arg_rev
+                rev_options = self.check_rev_options(dest, rev_options)
                 # Only do a checkout if the current commit id doesn't match
                 # the requested revision.
                 if not self.is_commit_id_equal(dest, rev_options.rev):
                     cmd_args = ['fetch', '-q', url] + rev_options.to_args()
                     self.run_command(cmd_args, cwd=dest)
                     self.run_command(
-                        ['checkout', '-q', 'FETCH_HEAD'],
+                        ['checkout', '-q', revision],
                         cwd=dest,
                     )
 
